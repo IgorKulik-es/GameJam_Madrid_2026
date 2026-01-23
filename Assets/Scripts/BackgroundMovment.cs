4dcb809ba3034ad59bda0fe213ca26a730e46a94
daliys <daliys133@gmail.com>
2026-01-23 16:57:33 +0100

    f

---
+++ Assets/Scripts/BackgroundMovment.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class BackgroundMovment : MonoBehaviour
+{
+
+    private SpriteRenderer _renderer;
+    
+    
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void Update()
+    {
+       // _renderer.
+    }
+}
