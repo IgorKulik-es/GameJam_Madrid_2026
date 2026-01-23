cd90803dc6e035543e4edc8926d0718adbc28887
daliys <daliys133@gmail.com>
2026-01-23 17:09:44 +0100

    added dotween

--- Assets/Scripts/BackgroundMovment.cs
+++ Assets/Scripts/BackgroundMovment.cs
@@ -1,20 +1,20 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class BackgroundMovment : MonoBehaviour
 {
-
+    [SerializeField] private float speed = 0.5f;
     private SpriteRenderer _renderer;
-    
     
     void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
     }
 
-
     private void Update()
     {
-       // _renderer.
+        
+        _renderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
     }
 }
